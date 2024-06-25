4216bca7b03b9edfa798c8bbbaf15973f011bf6b
Uladzimir Martynau <uladzimir.martynau@altruist.com>
2024-06-25 08:08:08 +0300

    Init commit

---
+++ src/main/java/com/mart/postebin/PostebinApplication.java
@@ -0,0 +1,13 @@
+package com.mart.postebin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostebinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostebinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mart/postebin/PostebinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mart.postebin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostebinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
